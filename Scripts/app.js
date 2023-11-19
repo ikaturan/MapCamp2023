4689f0a86bf3415079f8c0e58af286d9d8fdd77e
Sigma <sigma@dhcp-206-12-40-100.ubcsecure.wireless.ubc.ca>
2023-11-19 10:43:09 -0800

    adding 4 starting points

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,4 +12,43 @@ function myMap() {
 
   marker.setMap(map);
 
+  //varible NorthWest of ICICS @ Longitude: 49.262072, Latitude: -123.250419
+  var northWestLong = 49.262072;
+  var northWestLat = -123.250419;
+  const northWest = { lat: northWestLong, lng: northWestLat };
+
+  //varible NorthEast of ICICS @ Longitude: 49.2625, Latitude: -123.2474
+  var northEastLong = 49.2625;
+  var northEastLat = -123.2474;
+  const northEast = { lat: northEastLong, lng: northEastLat };
+
+  //varible SouthWest of ICICS @ Longitude: 49.259097, Latitude: -123.249079
+  var southWestLong = 49.259097;
+  var southWestLat = -123.249079;
+  const southWest = { lat: southWestLong, lng: southWestLat };
+
+  //varible SouthEast of ICICS @ Longitude: 49.260634, Latitude: -123.244685
+  var southEastLong = 49.260634;
+  var southEastLat = -123.244685;
+  const southEast = { lat: southEastLong, lng: southEastLat };
+
+  //marker for NorthWest Starting Point
+  var markerNW = new google.maps.Marker({ position: northWest });
+  markerNW.setMap(map);
+
+  //marker for NorthEast Starting Point
+  var markerNE = new google.maps.Marker({ position: northEast });
+  markerNE.setMap(map);
+
+  //marker for SouthWest Starting Point
+  var markerSW = new google.maps.Marker({ position: southWest });
+  markerSW.setMap(map);
+
+  //marker for SouthEast Starting Point
+  var markerSE = new google.maps.Marker({ position: southEast });
+  markerSE.setMap(map);
+
+  
+
+
 }
