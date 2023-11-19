d49e05631c1eeb85d3e1fda61eccb08af8464a8f
ikaturan <vincent.tu83@gmail.com>
2023-11-19 14:07:57 -0800

    moved objects to top level

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,35 @@ var directionfields = {from:49.2606, to:-123.2460}
 
 var direction = document.getElementById("direction")
 
+const mainEntrance = { lat: 49.26112013421764, lng: -123.24931284699397 };
+const leftEntrance = { lat: 49.260732, lng: -123.248916 };
+const rightEntrance = { lat: 49.26142733612942, lng: -123.24900841627249};
+const backEntranceLeft = { lat: 49.2611364, lng: -123.2483529 };
+const backEntranceRight = { lat: 49.2614651, lng: -123.248713 };
+
+const ICICS = {
+  
+  'Main Entrance':
+  { position: mainEntrance,
+    },
+    'Left Entrance':
+  { position: leftEntrance,
+     },
+  'Right Entrance':
+  { position: rightEntrance,
+     },
+  'Back Entrance left side':
+  { position: backEntranceLeft,
+     },
+  'Back Entrance right side':
+  { position: backEntranceRight,
+     },
+
+  
+
+  };
+
+  const locations = {'ICICS':ICICS}
 
 function myMap() {
   var mapProp = {
@@ -11,37 +40,9 @@ function myMap() {
   };
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
   const myCenter = { lat: 49.261199779342405, lng: -123.24890381012028 };
-
-  const mainEntrance = { lat: 49.26112013421764, lng: -123.24931284699397 };
-
-  const leftEntrance = { lat: 49.260732, lng: -123.248916 };
-  const rightEntrance = { lat: 49.26142733612942, lng: -123.24900841627249};
-  const backEntranceLeft = { lat: 49.2611364, lng: -123.2483529 };
-  const backEntranceRight = { lat: 49.2614651, lng: -123.248713 };
-
-  const ICICS = {
   
-    'Main Entrance':
-    { position: mainEntrance,
-      },
-      'Left Entrance':
-    { position: leftEntrance,
-       },
-    'Right Entrance':
-    { position: rightEntrance,
-       },
-    'Back Entrance left side':
-    { position: backEntranceLeft,
-       },
-    'Back Entrance right side':
-    { position: backEntranceRight,
-       },
 
-    
 
-    };
-
-  const locations = {'ICICS':ICICS}
 
   for (let building in locations) {
     let markers = locations[building];
