396e566ea250d2069fdd99da1a3c844d32ecfee9
ikaturan <vincent.tu83@gmail.com>
2023-11-19 09:33:24 -0800

    more tests

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,18 @@
 
     function myMap() {
     var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
+      center:new google.maps.LatLng(49.2606, -123.2460),
+      zoom:16,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    // Create a marker and set its position
+    var marker = new google.maps.Marker({
+        position: { lat: 49.2606, lng: -123.2460 },
+        map: map,
+        title: 'My Marker' // Optional title for the marker
+        
+      });
     }
 
     
