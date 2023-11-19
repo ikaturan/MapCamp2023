92e86cd2a3c6f9b8a41f9c893203e4b89f732993
jc-c1 <joannec.chang1@gmail.com>
2023-11-19 09:19:23 -0800

    ref'ed to js file in scripts and added a basic form

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,11 +1,15 @@
 
-    function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
 
-    
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCuayMV9gg8-0gIbDYxjPioTwjOkNH0ImI&callback=myMap"></script>
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(49.2606, -123.2460),
+    zoom: 17,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  const myCenter = { lat: 49.2606, lng: -123.2460 };
+
+  var marker = new google.maps.Marker({ position: myCenter });
+
+  marker.setMap(map);
+
+}
